5eab3565d7109ebd51703e511d01de603896b3a9
Christian Riedel <cr.mlist@googlemail.com>
2013-01-27 16:46:06 +0100

    adding module documentation

--- src/main/java/com/cupsoftware/carsharing/services/DevelopmentModule.java
+++ src/main/java/com/cupsoftware/carsharing/services/DevelopmentModule.java
@@ -3,6 +3,13 @@ package com.cupsoftware.carsharing.services;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 
+/**
+ * Run the application during development with the system parameter
+ * -Dtapestry.execution-mode=development to enable this module.
+ *
+ * @author criedel
+ * @see SymbolConstants#EXECUTION_MODE
+ */
 public class DevelopmentModule {
 
     public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration) {
